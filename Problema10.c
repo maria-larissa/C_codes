11ecf1ad9a31a4aeecc8f8465f8e256d0b4eaf3a
Maria Larissa <70605763+maria-larissa@users.noreply.github.com>
2022-04-10 16:08:37 -0300

    Update Problema10.c

--- Problema10.c
+++ Problema10.c
@@ -1,18 +1,19 @@
 /*  
-*    É a forma mais eficiente de calcular um fatorila, pois não exige muito da memória.
-*    Calcula o fatorial sem usar RECURSIVIDADE.
+*   É a forma mais eficiente de calcular um fatorila, pois não exige muito da memória.
+*   Calcula o fatorial sem usar RECURSIVIDADE.
+*   
+*       DEFINIÇÃO DE FATORIAL   
+*   n! = n * (n-1) * (n-2) * ... * 2 * 1
 */
 
 #include "stdio.h"
 
 int fat(int n){
-    if(n == 0 || n ==1){
+    if(n == 0 || n ==1){        //Caso n seja um dos casos bases.
         return 1;
     }
-    //
-    //
     int resultado = n, i = n;
-    while( i > 1){
+    while( i > 1){              //Caso contrário, vai calculando cada parcela do produtório e já armazenando o resultado em um variável.
         i--;
         resultado *= i;
     }
