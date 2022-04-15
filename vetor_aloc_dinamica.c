170e042ad762fb0629330af4e0d97ffa89606224
Maria Larissa <70605763+maria-larissa@users.noreply.github.com>
2022-04-15 17:48:04 -0300

    Update vetor_aloc_dinamica.c

--- vetor_aloc_dinamica.c
+++ vetor_aloc_dinamica.c
@@ -1,24 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+/*
+*   O algoritmo tem objetivo de alocar dinamicamente vetores dinamicamente
+*/
 int main(){
     int n;
     printf("digite o tamanho do vetor: ");
     scanf("%d", &n);
 
-    //alocando um vetor de inteiro dinamicamente
-    /*(int*) ---> cast para a alocação retornar um ponteiro de inteiros
-     n*sizeof(int) ---> será alocada memoria do tamanho de int n vezes*/
     int *vet = (int*)malloc(n*sizeof(int));
+    
     printf("\ndigite os valores\n");
     for(int i=0; i<n; i++){
-        //não é preciso usar "&" apenas o nome do ponteiro + constante
-        //(vet+i) é o endereço da posição, uso para colocar valores dentro do vetor
         scanf("%d", (vet+i));
     }
+    
     printf("\nVetor:");
     for(int i=0; i<n;i++){
-        //vet[i] é o valor dentro da posição, uso pra acessar valores dentro do vetor
         printf("%3d", vet[i]);
     }
     printf("\n");
